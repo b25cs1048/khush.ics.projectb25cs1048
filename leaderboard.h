8db84c661c2b79ad322090414b9f5990b8534854
b25cs1048 <b25cs1048@iitj.ac.in>
2026-04-20 16:03:04 +0530

    Update leaderboard.h

--- leaderboard.h
+++ leaderboard.h
@@ -1,27 +1,9 @@
-// #ifndef LEADERBOARD_H
-// #define LEADERBOARD_H
-
-// #include <iostream>
-// #include <fstream>
-// #include <vector>
-// #include <string>
-// using namespace std;
-
-// class Leaderboard
-// {
-// public:
-//     bool hasAttempted(string username);
-//     int getUserScore(string username);
-//     void totalPlayers();
-//     void saveScore(string username, int score, int total);
-//     void showLeaderboard();
-//      void showLeaderboard(int totalQuestions);
-// };
-
-// #endif
 #ifndef LEADERBOARD_H
 #define LEADERBOARD_H
 
+#include <iostream>
+#include <fstream>
+#include <vector>
 #include <string>
 using namespace std;
 
@@ -29,12 +11,11 @@ class Leaderboard
 {
 public:
     bool hasAttempted(string username);
+    int getUserScore(string username);
     void totalPlayers();
-
-    // Save score with total questions
     void saveScore(string username, int score, int total);
-
     void showLeaderboard();
+     void showLeaderboard(int totalQuestions);
 };
 
 #endif
